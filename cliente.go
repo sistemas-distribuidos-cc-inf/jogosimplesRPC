fde65639970acf458985804cac2954132a806435
sidneyfrancois <sidney.fvm@hotmail.com>
2017-11-09 20:07:35 -0200

    Adicionando componentes de conexao no servidor e estruturas que irao se comunicar

--- cliente.go
+++ cliente.go
@@ -6,6 +6,17 @@ import (
 	"net/rpc"
 )
 
+type JogoDaVelha struct {
+	Connection *rpc.Client
+}
+
+type EstadoDoJogo struct {
+	Tabuleiro [][]string
+	Marca     string
+	Linha     int
+	Coluna    int
+}
+
 func main() {
 
 }
--- servidor.go
+++ servidor.go
@@ -6,6 +6,30 @@ import (
 	"net/rpc"
 )
 
-func main() {
+type JogoDaVelha struct {
+	Tabuleiro [][]string
+	Marca     string
+	Linha     int
+	Coluna    int
+}
 
+type Jogo int
+
+func main() {
+	jogo := new(Jogo)
+	rpc.Register(jogo)
+
+	ln, err := net.Listen("tcp", ":1234")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	for {
+		c, err := ln.Accept()
+		if err != nil {
+			continue
+		}
+		go rpc.ServeConn(c)
+	}
 }
