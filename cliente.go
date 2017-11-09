2988d63d1310d3621154d3fb82b7eeb94f73d34a
sidneyfrancois <sidney.fvm@hotmail.com>
2017-11-09 20:44:20 -0200

    Adicionando funcoes ao cliente

--- cliente.go
+++ cliente.go
@@ -26,6 +26,55 @@ func (t *EstadoDoJogo) AlocaTabuleiro() {
 	}
 }
 
+func (t *JogoDaVelha) Inserir(args *EstadoDoJogo) {
+
+	fmt.Println("Digite a linha: ")
+	fmt.Scanf("%d", &args.Linha)
+	fmt.Println("Digite a coluna: ")
+	fmt.Scanf("%d", &args.Coluna)
+
+	if (args.Linha <= 2) || (args.Linha <= 0) {
+		err := t.Connection.Call("Jogo.Marcar", args, &args)
+		if err != nil {
+			log.Fatal("erro no jogo1: ", err)
+
+		}
+
+	} else {
+		fmt.Println("linha ou coluna fora invalidas")
+
+	}
+
+}
+
+func (t *JogoDaVelha) ImprimirJogo(args *EstadoDoJogo) {
+
+	var resposta int
+
+	err := t.Connection.Call("Jogo.ImprimeTabuleiro", args, &resposta)
+	if err != nil {
+		log.Fatal("erro no jogo: ", err)
+
+	}
+
+}
+
+func (t *JogoDaVelha) VerificarVencedor(args *EstadoDoJogo) bool {
+
+	var verificador bool
+
+	err := t.Connection.Call("Jogo.VerificaVencedor", args, &verificador)
+	if err != nil {
+		log.Fatal("erro no jogo3: ", err)
+
+	}
+
+	fmt.Println(verificador)
+
+	return verificador
+
+}
+
 func main() {
 
 }
